18de5ad2ac540ef8650629ef165afbcfd39a9d5c
Ginaries <155573435+Ginaries@users.noreply.github.com>
2025-05-27 00:40:49 -0300

    Initial commit

---
+++ Scene/NPC/npc.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var Name:String
+var Speak:bool=false
+const DIALOGO_INICIAL = preload("res://Dialogos/Dialogo Inicial.dialogue")
+var player:CharacterBody2D
+
+
+
+func Hablar():
+	DialogueManager.show_dialogue_balloon(DIALOGO_INICIAL)
+
+
+func _on_detectar_player_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player=body
+
+
+
+func _on_detectar_player_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player=null
